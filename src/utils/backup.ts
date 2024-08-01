d6bd841d9550fedc3701d0a5406278badc9dd4fc
hemancini <hernan.mancini@gmail.com>
2024-08-01 16:26:09 -0400

    refactor: Update backup encryption and decryption methods

--- src/utils/backup.ts
+++ src/utils/backup.ts
@@ -1,21 +1,18 @@
 import type { OTPEntry } from "@src/entry/type";
+import { decrypData, encrypData } from "@src/utils/crypto";
 import { getBackgroundEntries } from "@src/utils/entry";
-import CryptoJS from "crypto-js";
-
-const appKey = import.meta.env.VITE_APP_KEY || DEFAULT_APP_KEY;
 
 export const exportBackup = async (): Promise<Blob> => {
   const entries = (await getBackgroundEntries()) ?? new Map();
   if (entries.size === 0) throw new Error("No data found", { cause: "notEntriesFound" });
-
-  const encrypted = CryptoJS.AES.encrypt(JSON.stringify(Array.from(entries?.values() ?? [])), appKey).toString();
-  const dataBlob = new Blob([JSON.stringify({ data: encrypted }, null, 2)], { type: "application/json" });
-  return dataBlob;
+  const encryptedData = encrypData(JSON.stringify(Array.from(entries?.values() ?? [])));
+  const blobData = new Blob([JSON.stringify({ data: encryptedData }, null, 2)], { type: "application/json" });
+  return blobData;
 };
 
 export const decryptBackup = async ({ data }: { data: string }) => {
-  const entriesString = CryptoJS.AES.decrypt(data, appKey).toString(CryptoJS.enc.Utf8);
-  const entriesArray = JSON.parse(entriesString) as OTPEntry[];
+  const decryptedData = decrypData(data);
+  const entriesArray = JSON.parse(decryptedData) as OTPEntry[];
   const entries = new Map(entriesArray.map((entry) => [entry.hash, entry]));
   return entries;
 };
